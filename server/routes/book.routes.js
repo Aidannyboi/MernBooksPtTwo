d0f39edb1e6f3718bdc19e029f800fc119be5055
Aidan Castellanos <castellanosaidan@gmail.com>
2024-02-18 15:08:00 -0800

    Improved spacing for QOL

--- server/routes/book.routes.js
+++ server/routes/book.routes.js
@@ -1,11 +1,16 @@
 import {Router} from "express";
+import { createBook, deleteOneBook, getAllBooks, getOneBook, updateOneBook } from "../controllers/book.controller";
 
 const router = Router()
 
 router.route("/books")
-
+    .get(getAllBooks)
+    .post(createBook)
 
 router.route("/book/:id")
+    .get(getOneBook)
+    .put(updateOneBook)
+    .delete(deleteOneBook)
 
 
 export default router
