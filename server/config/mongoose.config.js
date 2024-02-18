19d658a187cc8d13fdd4d1303479d22d95a4c0de
Aidan Castellanos <castellanosaidan@gmail.com>
2024-02-18 14:15:43 -0800

    Added config for DB and then allowed connection to

---
+++ server/config/mongoose.config.js
@@ -0,0 +1,23 @@
+import {connect} from 'mongoose';
+
+import dotenv from 'dotenv';
+
+dotenv.config();
+
+const MONGODB_URI = process.env.MONGODB_URI;
+
+async function dbConnect() {
+    try {
+        await connect (MONGODB_URI, {
+            dbName: 'booksMernDB',
+        });
+
+        console.log("I have succesfully connected to the database in MongoDB!")
+    } catch(error) {
+        console.log(error);
+        throw error;
+    }
+
+}
+
+export default dbConnect;
--- server/server.js
+++ server/server.js
@@ -1,2 +1,24 @@
+import express from "express";
+
 import cors from'cors';
+
+import dotenv from "dorenv";
+
+import dbConnect from "./config/mongoose.config";
+
+
+
+const app = express();
+
+app.use(express.json(), cors());
+
+app.use('/api', router);
+
+dotenv.config();
+
+const PORT = process.env.PORT;
+
+dbConnect();
+
+app.listen(PORT, () => console.log("Listening on port 9999"));
 
